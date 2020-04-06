67eea3747b52286b96af8536a5298fab325235f3
networks <18628094@qq.com>
2020-04-06 17:40:17 +0800

    第一此提交

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+		<!-- 第三步使用组件 -->
+		<tabbar>
+			
+		</tabbar>
+		
+  </div>
+</template>
+
+<script>
+//第一步导入组件
+	import tabbar from './components/tabbar/tabbar.vue'
+
+
+
+export default {
+  name: 'app',
+  components: {
+		// 第二步注册组件
+		
+				tabbar
+  }
+}
+</script>
+
+<style>
+	@import url("./assets/css/base.css");
+	
+</style>
---
+++ src/components/tabbar/tabbar.vue
@@ -0,0 +1,30 @@
+<template>
+	<div id="tabbar">
+						 <div class="tabbar-item">首页</div>
+						 <div class="tabbar-item">购物车</div>
+						 <div class="tabbar-item">首页</div>
+						 <div class="tabbar-item">我的</div>
+				 </div>   
+</template>
+
+<script>
+</script>
+
+<style scoped="scoped">
+	#tabbar{
+			display: flex;
+			background-color: #ffaa7f;
+			left: 0;
+			right: 0;
+			height: 3.0625rem;
+			bottom: 0;
+			box-shadow: 0px,-3px,3px,rgba(100,100,100,.8);
+			position: fixed;
+			}
+		.tabbar-item{
+			flex: 1;
+			text-align: center;
+		
+		}
+
+</style>
